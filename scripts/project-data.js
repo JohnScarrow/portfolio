13fc1eb64a8b1322961af6bc795069d58e10c5d5
JohnScarrow <johna@scarrows.com>
2017-05-13 01:15:04 -0700

    day-01

---
+++ scripts/project-data.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var projects = [
+  {
+    title:       'Bus Mall',
+    link:        'github.io',
+    description: '<p>this is the shizz</p>'
+  },
+  {
+    title:       'Bus Mall',
+    link:        'github.io',
+    description: '<p>this is the shizz</p>'
+  },
+  {
+    title:       'Bus Mall',
+    link:        'github.io',
+    description: '<p>this is the shizz</p>'
+  },
+];
