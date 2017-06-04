bd98143414f0a44b006dfbda651f1a0bf3731251
JohnScarrow <johna@scarrows.com>
2017-06-04 01:31:01 -0700

    fixed hamburger menu and reset

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,21 @@
 'use strict';
 var projects = [];
 
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
 
 function Project (projectsObj) {
   this.title = projectsObj.title;
